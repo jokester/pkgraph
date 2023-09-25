c92fa2115cc9d2ff27080df6845c4b0b8fb21ae2
Wang Guan <momocraft@gmail.com>
2023-09-26 03:17:59 +0900

    wip

---
+++ src/graph/index.ts
@@ -0,0 +1,10 @@
+import { Lockfile } from 'snyk-nodejs-lockfile-parser/dist/parsers';
+
+class Graph {
+  static buildGraph(lockfile: Lockfile): Graph {
+    return new Graph();
+  }
+
+
+
+}
--- src/loader/lockfileLoader.ts
+++ src/loader/lockfileLoader.ts
@@ -1,4 +1,4 @@
-import snykLockFileParser from 'snyk-nodejs-lockfile-parser/dist/parsers';
+import snykLockFileParser, { parseManifestFile, ManifestFile } from 'snyk-nodejs-lockfile-parser/dist/parsers';
 import { YarnLockParser } from 'snyk-nodejs-lockfile-parser/dist/parsers/yarn-lock-parser';
 import { Yarn2LockParser } from 'snyk-nodejs-lockfile-parser/dist/parsers/yarn2-lock-parser';
 import { PackageLockParser as NpmLockParser } from 'snyk-nodejs-lockfile-parser/dist/parsers/package-lock-parser';
@@ -27,4 +27,8 @@ export async function loadLockfile(content: string): Promise<snykLockFileParser.
     logger('Failed to parse as npm lockfile', ignored);
   }
   throw new Error('unsupported lock file');
+}
+
+export async function tryParseManifestFile(content: string): Promise<ManifestFile> {
+  return parseManifestFile(content);
 }
