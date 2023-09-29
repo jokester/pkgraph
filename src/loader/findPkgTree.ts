cba1d725d6b9cd515d23f61d98e67267c9a5e1a8
Wang Guan <momocraft@gmail.com>
2023-09-29 22:24:05 +0900

    wip

--- src/loader/findPkgTree.ts
+++ src/loader/findPkgTree.ts
@@ -1,9 +1,17 @@
 import fsp from 'node:fs/promises';
 import path from 'node:path';
-import { buildDepTreeFromFiles, PkgTree } from 'snyk-nodejs-lockfile-parser';
+import {
+  buildDepTree,
+  buildDepTreeFromFiles,
+  parseNpmLockV2Project,
+  parseYarnLockV2Project,
+  PkgTree,
+} from 'snyk-nodejs-lockfile-parser';
+import { DepGraph } from '@snyk/dep-graph';
 
 interface ResolvedManifest {
   pkgTree: PkgTree;
+  depGraph: DepGraph;
 }
 
 async function isFile(filename: string): Promise<boolean> {
@@ -17,20 +25,45 @@ async function isFile(filename: string): Promise<boolean> {
  */
 export async function findPkgTree(start: string): Promise<ResolvedManifest> {
   const manifestPath = path.join(start, 'package.json');
-  if (!(await isFile(manifestPath))) {
+  const manifestContent = await fsp.readFile(manifestPath, { encoding: 'utf8' }).catch(() => null);
+  if (!manifestContent) {
     throw new Error(`package.json not found`);
   }
 
   const npmLockPath = path.join(start, 'package-lock.json');
-  if (await isFile(npmLockPath)) {
-    const pkgTree = await buildDepTreeFromFiles(start, 'package.json', 'package-lock.json', true);
-    return { pkgTree };
+  const npmLockContent = await fsp.readFile(npmLockPath, { encoding: 'utf8' }).catch(() => null);
+  if (npmLockContent) {
+    const pkgTree = await buildDepTree(manifestContent, npmLockContent, true);
+    if (pkgTree.meta?.packageManager === 'npm' && pkgTree.meta.lockfileVersion === 2) {
+      const depGraph = await parseNpmLockV2Project(manifestContent, npmLockContent, {
+        // "any cross referencing between workspace packages will lead to a throw"
+        strictOutOfSync: false,
+        includeDevDeps: true,
+        includeOptionalDeps: true,
+        // maybe only affact yarn v1
+        pruneCycles: true,
+      });
+      return {
+        pkgTree,
+        depGraph,
+      };
+    }
   }
 
   const yarnLockPath = path.join(start, 'yarn.lock');
-  if (await isFile(yarnLockPath)) {
-    const pkgTree = await buildDepTreeFromFiles(start, 'package.json', 'yarn.lock', true);
-    return { pkgTree };
+  const yarnLockContent = await fsp.readFile(yarnLockPath, { encoding: 'utf8' }).catch(() => null);
+  if (yarnLockContent) {
+    const pkgTree = await buildDepTree(manifestContent, yarnLockContent, true);
+    if (pkgTree.meta?.packageManager === 'yarn' && pkgTree.meta.lockfileVersion === 2) {
+      const depGraph = await parseYarnLockV2Project(manifestContent, yarnLockContent, {
+        includeDevDeps: true,
+        includeOptionalDeps: true,
+        strictOutOfSync: true,
+        // TODO: what does this do?
+        pruneWithinTopLevelDeps: false,
+      });
+      return { pkgTree, depGraph };
+    }
+    throw new Error(`unable to build PkgTree `);
   }
-  throw new Error(`unable to build PkgTree `);
 }
